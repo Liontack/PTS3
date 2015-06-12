5f3c0beb8b86fca719c13cad9921d63e4438d4eb
Liontack <Liontack@live.nl>
2015-06-12 15:32:53 +0200

    Cleanup in the unittests
    Test completely green again

--- Pong spel V2/test/model/GameManagementTest.java
+++ Pong spel V2/test/model/GameManagementTest.java
@@ -2,24 +2,30 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.rmi.RemoteException;
+
 import org.junit.Test;
 
 public class GameManagementTest{
 	
 	@Test
 	public void testJoinGame(){
-		User a = UserManagement.addUser("a", "a");
-		User b = UserManagement.addUser("b", "b");
-		User c = UserManagement.addUser("c", "c");
-		User d = UserManagement.addUser("d", "d");
-		
-		// Test when players can join a game
-		assertTrue(GameManagement.joinGame(a));
-		assertTrue(GameManagement.joinGame(b));
-		assertTrue(GameManagement.joinGame(c));
-		assertTrue(GameManagement.joinGame(d));
-		assertFalse(GameManagement.joinGame(a));
-		assertTrue(GameManagement.startGame(b.getPlayer()));
+		try{
+			int a = UserManagement.getInstance().registerUser("a", "a");
+			int b = UserManagement.getInstance().registerUser("b", "b");
+			int c = UserManagement.getInstance().registerUser("c", "c");
+			int d = UserManagement.getInstance().registerUser("d", "d");
+			
+			// Test when players can join a game
+			assertTrue(GameManagement.getInstance().joinGame(a) != 0);
+			assertTrue(GameManagement.getInstance().joinGame(b) != 0);
+			assertTrue(GameManagement.getInstance().joinGame(c) != 0);
+			assertTrue(GameManagement.getInstance().joinGame(d) != 0);
+			assertTrue(GameManagement.getInstance().joinGame(a) == 0);
+			assertTrue(GameManagement.getInstance().startGame(b));
+		}catch(RemoteException exception){
+			fail("Remote Exception was thrown");
+		}
 	}
 	
 }
--- Pong spel V2/test/model/UserManagementTest.java
+++ Pong spel V2/test/model/UserManagementTest.java
@@ -1,43 +1,58 @@
 package model;
 
 import static org.junit.Assert.*;
+
+import java.rmi.RemoteException;
+
 import org.junit.*;
 
 public class UserManagementTest{
 	
 	@Test
 	public void testAdd(){
-		// Test the registration of users
-		int userAmount = UserManagement.getUsers().size();
-		assertNotNull(UserManagement.addUser("Programmer", "root"));
-		assertEquals(++userAmount, UserManagement.getUsers().size());
-		assertNull(UserManagement.addUser("Programmer", "also_root"));
-		assertEquals(userAmount, UserManagement.getUsers().size());
-		assertNotNull(UserManagement.addUser("Other programmer", "root"));
-		assertEquals(++userAmount, UserManagement.getUsers().size());
+		try{
+			// Test the registration of users
+			int userAmount = UserManagement.getUsers().size();
+			assertTrue(UserManagement.getInstance().registerUser("Programmer", "root") != 0);
+			assertEquals(++userAmount, UserManagement.getUsers().size());
+			assertTrue(UserManagement.getInstance().registerUser("Programmer", "also_root") == 0);
+			assertEquals(userAmount, UserManagement.getUsers().size());
+			assertTrue(UserManagement.getInstance().registerUser("Other programmer", "root") != 0);
+			assertEquals(++userAmount, UserManagement.getUsers().size());
+		}catch(RemoteException exception){
+			fail("Remote Exception was thrown");
+		}
 	}
 	
 	@Test
 	public void testLogin(){
-		// Test the login of users
-		assertNull(UserManagement.userLogin("Person a", "a"));
-		User a = UserManagement.addUser("Person a", "a");
-		assertNull(UserManagement.userLogin("Person a", "a"));
-		assertTrue(UserManagement.isUserLoggedIn(a));
-		UserManagement.userLogout(a);
-		assertFalse(UserManagement.isUserLoggedIn(a));
-		assertNull(UserManagement.userLogin("Person a", "b"));
-		assertNotNull(UserManagement.userLogin("person a", "a"));
-		assertNull(UserManagement.userLogin("Person a", "a"));
-		assertTrue(UserManagement.isUserLoggedIn(a));
+		try{
+			// Test the login of users
+			assertTrue(UserManagement.getInstance().userLogin("Person a", "a") == 0);
+			int a = UserManagement.getInstance().registerUser("Person a", "a");
+			assertTrue(UserManagement.getInstance().userLogin("Person a", "a") == 0);
+			assertTrue(UserManagement.isUserLoggedIn(UserManagement.getUserByID(a)));
+			UserManagement.getInstance().userLogout(a);
+			assertFalse(UserManagement.isUserLoggedIn(UserManagement.getUserByID(a)));
+			assertTrue(UserManagement.getInstance().userLogin("Person a", "b") == 0);
+			assertTrue(UserManagement.getInstance().userLogin("person a", "a") != 0);
+			assertTrue(UserManagement.getInstance().userLogin("Person a", "a") == 0);
+			assertTrue(UserManagement.isUserLoggedIn(UserManagement.getUserByID(a)));
+		}catch(RemoteException exception){
+			fail("Remote Exception was thrown");
+		}
 	}
 	
 	@Test
 	public void testRatings(){
-		// Test if each user is producing an RatingWrapper
-		assertEquals(UserManagement.getUsers().size(), UserManagement.getUserRatings().size());
-		UserManagement.addUser("test person for ratings", "r");
-		assertEquals(UserManagement.getUsers().size(), UserManagement.getUserRatings().size());
+		try{
+			// Test if each user is producing an RatingWrapper
+			assertEquals(UserManagement.getUsers().size(), UserManagement.getInstance().getUserRatings().size());
+			UserManagement.getInstance().registerUser("test person for ratings", "r");
+			assertEquals(UserManagement.getUsers().size(), UserManagement.getInstance().getUserRatings().size());
+		}catch(RemoteException exception){
+			fail("Remote Exception was thrown");
+		}
 	}
 	
 }
